dfb32809593f3142addf487667345e5c9378f2e6
antidote-kt <140964493+antidote-kt@users.noreply.github.com>
2025-04-10 15:20:21 +0000

    发布在windows运行docker博客 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/03/hello-world/","2025/03/31/post/"];function toRandomPost(){
+var posts=["2025/04/10/docker/","2025/04/10/hello-world/","2025/03/31/post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
